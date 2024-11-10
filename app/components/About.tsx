83cbed9886afb92bf0396b90a6b59b8c850080d3
HANIA IMRAN <haniaimran488@gmail.com>
2024-11-10 06:46:20 -0800

    next

---
+++ app/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='text-emerald-950 text-1xl m-2 font-serif'>
+            <h2> This is About </h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+ 
+const Header = () => {
+    return (
+        <div>
+         <h1 className='text-yellow-950 text-1xl m-2 from-neutral- font-serif '> This is Header </h1>
+        </div>
+    );
+};
+
+export default Header;
+
+
